8f739d207fedd58004082d93b2b44ac571e87c5d
Anas Ahmad <ahmadanas4490@gmail.com>
2021-08-24 19:20:54 +0500

    styling with bootstrap and started association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true ,length: { minimum:3, maximum: 30 }
+  belongs_to :user
+  validates :title, presence: true,
+            length: { minimum:3, maximum: 30 }
   validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,13 @@
-class User < ApplicationRecord;git  end
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  has_many :articles
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_RAGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 25 },
+                    format: { with: VALID_EMAIL_RAGEX }
+  has_secure_password
+end
